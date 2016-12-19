30b6119b56f5444f9c8cb50bc79bebace41949c3
mttstt <mtt_stt@yahoo.it>
2016-12-19 12:33:25 +0100

    Delete click-1.3.4.1-NoMeteor.bat

--- click-1.3.4.1-NoMeteor.bat
+++
@@ -1,7 +0,0 @@
-del /Q Domoticasite.json
-rmdir Domoticasite /S /Q
-
-rename Domo*.json Domoticasite.json   
-meteor-kitchen ./Domoticasite.json ./Domoticasite --meteor-release 1.3.4.1  2>io.txt
-cd ./Domoticasite
-               
