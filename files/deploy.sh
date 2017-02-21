9b3a5c4d3c3bd02a670c8777b8bb298e877ba8e6
mttstt <mtt_stt@yahoo.it>
2017-02-21 21:35:12 +0100

    Commit 0.0.3
    Commit

--- files/deploy.sh
+++ files/deploy.sh
@@ -1,12 +1,14 @@
 kk#!/bin/bash
-echo date | tee /home/pi/deploy.log
+echo $(date) | tee /home/pi/deploy.log
 /home/pi/.nvm/versions/node/v6.7.0/bin/forever stop 0
 rm -rf /home/pi/DomoticasiteP
 mv /home/pi/Domoticasitetmp /home/pi/DomoticasiteP
+cd /home/pi/.forever
+rm *.log
 cd /home/pi/DomoticasiteP/programs/server
-npm install --save wiring-pi
-npm install --save git+https://github.com/HardwareProjects/node-dht-sensor.git
 npm install
 npm install --save wiring-pi
-/home/pi/.nvm/versions/node/v6.7.0/bin/forever start /home/pi/DomoticasiteP/main.js
+npm install --save git+https://github.com/HardwareProjects/node-dht-sensor.git
+cd /home/pi/DomoticasiteP
+/home/pi/.nvm/versions/node/v6.7.0/bin/forever start main.js
 
